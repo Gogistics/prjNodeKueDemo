8d17629af5e3fade60283b0601776272f0c83a12
Alan Tai <gogistics.tw@gmail.com>
2016-08-14 09:51:46 -0700

    update redis config. for production

--- script/production.sh
+++ script/production.sh
@@ -1,1 +1,1 @@
-NODE_ENV=staging REDIS_PORT=6379 REDIS_HOST=localhost REDIS_PASS=nodeKue168 forever start .
+NODE_ENV=staging REDIS_PORT=6378 REDIS_HOST=localhost REDIS_PASS=nodeKue168 forever start .
